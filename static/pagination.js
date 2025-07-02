a8381a68676d3cfae427c3231c12cd48d0ca11c7
Adam Konrad <git@adamkonrad.com>
2025-07-02 11:04:45 +0200

    prevent click event on disabled pagination links

--- static/pagination.js
+++ static/pagination.js
@@ -16,7 +16,12 @@ class Pagination {
         paginationLinks.forEach((link) => {
             link.addEventListener('click', (event) => {
                 event.preventDefault();
-                const page = event.target.getAttribute('data-page');
+                const target = event.target;
+                const disabled = target.hasAttribute('disabled');
+                if(disabled) {
+                    return;
+                }
+                const page = target.getAttribute('data-page');
                 this.page = parseInt(page, 10);
                 this.grid.load();
             });
