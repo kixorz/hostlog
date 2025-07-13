0bfdda088020693013c74fbb452a8291b9d7aa50
Adam Konrad <git@adamkonrad.com>
2025-07-13 12:06:23 +0200

    add TCP support to syslog server and update startup message

--- main.go
+++ main.go
@@ -24,13 +24,14 @@ func main() {
 	server := syslog.NewServer()
 	server.SetFormat(syslog.RFC3164)
 	server.SetHandler(handler)
+	server.ListenTCP("0.0.0.0:514")
 	server.ListenUDP("0.0.0.0:514")
 	err = server.Boot()
 	if err != nil {
 		log.Fatalf("Failed to start syslog server: %v", err)
 	}
 
-	fmt.Println("Syslog server started. Listening on UDP port 514...")
+	fmt.Println("Syslog server started. Listening on port 514...")
 
 	// Process incoming log messages
 	go func(channel syslog.LogPartsChannel) {
